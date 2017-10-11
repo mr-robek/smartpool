26a78497adccce3dfa2a1d7d3eded20579fa3320
mr-robek <koprbor@gmail.com>
2017-10-10 22:24:07 -0400

    initial Pool.sol version 0.0.0.0.1

--- test/pool.js
+++ test/pool.js
@@ -1,10 +1,10 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Pool = artifacts.require("./Pool.sol");
 
-contract('SimpleStorage', function(accounts) {
+contract('Pool', function(accounts) {
 
   it("...should store the value 89.", function() {
-    return SimpleStorage.deployed().then(function(instance) {
-      simpleStorageInstance = instance;
+    return Pool.deployed().then(function(instance) {
+      poolInstance = instance;
 
       return simpleStorageInstance.set(89, {from: accounts[0]});
     }).then(function() {
